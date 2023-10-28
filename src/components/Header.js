bd3e083fb501215b84fc47fd5c1ac6dcf34f95ac
Vũ Trường Khoa <135007011+VuTruongKhoa@users.noreply.github.com>
2023-10-29 00:02:36 +0700

    modify file /components/Header.js and adjust some file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,23 @@ import React from "react";
 
 export default function Header(){
     return(
-        <h1>this is Header</h1>
+        <header className="header">
+            <img src = "header.png" className="img--header"/>
+            <div className="header--name">
+                <p>HỆ THỐNG QUẢN LÝ THƯ VIỆN</p>
+                <p>Library Management System</p>
+                <p className="normal-text">Design by Syntax Terminators</p>
+            </div>
+            <div className="header--user">
+                <div>
+                    <h3><a>User 123</a></h3>
+                    <a><i className="ti-bell large--icon"></i></a>
+                </div>
+                <div>
+                    <img src = "user.png" className="User-img"/>
+                    <a><h4>Đăng xuất</h4></a>
+                </div>
+            </div>
+        </header>
     )
 }
--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../components/Footer"
 import Contact from "../components/Contact"
+import Header from "../components/Header"
 
 export default function Home(){
     return(
         <div>
             <h1>this is Home page</h1>
+            <Header />
             <Contact/>
             <Footer/>
         </div>
