c5651f3b5a9861cc8a0ca7778c628d06ad8f776a
Vũ Trường Khoa <135007011+VuTruongKhoa@users.noreply.github.com>
2023-11-04 11:21:58 +0700

    cap nhat

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,40 @@
 import React from "react";
+import './Header.css';
 
 export default function Header(){
     return(
-        <header className="header">
-            <img src = "header.png" className="img--header"/>
-            <div className="header--name">
-                <p>HỆ THỐNG QUẢN LÝ THƯ VIỆN</p>
-                <p>Library Management System</p>
-                <p className="normal-text">Design by Syntax Terminators</p>
+        <div className="header">
+            <div>
+                <h1>LIBRARY</h1>
             </div>
-            <div className="header--user">
-                <div>
-                    <h3><a>User 123</a></h3>
-                    <a><i className="ti-bell large--icon"></i></a>
-                </div>
-                <div>
-                    <img src = "user.png" className="User-img"/>
-                    <a><h4>Đăng xuất</h4></a>
-                </div>
+            <div className="nav-bar">
+                <input className="search-input" type="text" placeholder="Tìm kiếm một cuốn sách"></input>
+                <ul className="nav-options">
+                    <li className="option"> Trang chủ </li>
+                    <li className="option"> Sách </li>
+                    <li className="option"> Đăng nhập </li>
+                </ul>
             </div>
-        </header>
+
+        </div>
+        
+        // <header className="header">
+        //     <img src = "header.png" className="img--header"/>
+        //     <div className="header--name">
+        //         <p>HỆ THỐNG QUẢN LÝ THƯ VIỆN</p>
+        //         <p>Library Management System</p>
+        //         <p className="normal-text">Design by Syntax Terminators</p>
+        //     </div>
+        //     <div className="header--user">
+        //         <div>
+        //             <h3><a>User 123</a></h3>
+        //             <a><i className="ti-bell large--icon"></i></a>
+        //         </div>
+        //         <div>
+        //             <img src = "user.png" className="User-img"/>
+        //             <a><h4>Đăng xuất</h4></a>
+        //         </div>
+        //     </div>
+        // </header>
     )
 }
