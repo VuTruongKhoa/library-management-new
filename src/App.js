4425870c33f2640b9734593465e6a851b5ea8893
Vũ Trường Khoa <135007011+VuTruongKhoa@users.noreply.github.com>
2023-12-04 07:49:10 +0700

    update

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
             <Route path='/book-extend' element ={<BookExtend/>}></Route>
             <Route path='/book-regis' element ={<BookRegis/>}></Route>
         </Routes>
+        {/* <AddBookPage/> */}
       </div>
   );
 }
