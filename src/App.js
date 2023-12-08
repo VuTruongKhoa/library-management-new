608f90be3dcb3e851a0c8a876b7436beadb07227
Vũ Trường Khoa <135007011+VuTruongKhoa@users.noreply.github.com>
2023-12-08 22:14:49 +0700

    modify

--- src/App.js
+++ src/App.js
@@ -11,22 +11,24 @@ import BookRegis from './pages/book-register';
 import Infor from './pages/Infor';
 import BookExtend from './pages/book-extend';
 import ViewBookPage from './pages/view-book-page';
-
+import ManageMemberPage from './pages/manage-member-page';
+import AdminHeader from './components/Admin/AdminHeader';
 
 
 function App() {
   return (
       <div>
-        <Header/>
-        {/* <Routes>
+        <AdminHeader/>
+        <Routes>
             <Route path="/" element= {<Home/>}/>
             <Route  path='/login' element = {<Login/>} />
             <Route  path='/login' element = {<Login/>}></Route>
             <Route path ='/infor' element ={<Infor/>}></Route>
             <Route path='/infor/book-extend' element ={<BookExtend/>}></Route>
             <Route path='/infor/book-regis' element ={<BookRegis/>}></Route>
-        </Routes> */}
-        <ViewBookPage/>
+            <Route path='/adminhome/addnewbook' element ={<AddBookPage/>}></Route>
+            <Route path='/adminhome/addmember' element ={<ManageMemberPage/>}></Route>
+        </Routes>
       </div>
   );
 }
